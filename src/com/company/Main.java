3534737f1b1fd14fccdac725438ff18dd7cb5e67
jkurian <jkurian@usc.edu.au>
2021-03-14 19:46:18 +1000

    Revised program commited for random mumbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
+import java.util.Random;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world");
+    public static int getRandomInRange(int start, int end) {
+        Random generator = new Random();
+        return (start + generator.nextInt(end - start + 1));
     }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            int random = getRandomInRange(1, 5);
+            System.out.print(random + " ");
+        }
+
+
+    }
+
 }
